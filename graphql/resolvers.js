9d814ebde1948a12ed72b8bc292a853db1683f0c
Steveen <steveen.echeverri@gmail.com>
2021-01-04 16:15:12 -0500

    Created migration chatdb in mysql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,21 +1,15 @@
+const { User } = require('../models');
+
 module.exports = {
     Query: {
-        getUsers: () => {
-            const users = [
-                {
-                    username: "User1",
-                    email: "user1@gmail.com"
-                },
-                {
-                    username: "User2",
-                    email: "user2@gmail.com"
-                },
-                {
-                    username: "User3",
-                    email: "user3@gmail.com"
-                }
-            ]
-            return users;
+        getUsers: async () => {
+            try {
+                const users = await User.findAll()
+
+                return users
+            } catch(err) {
+                console.log(err);
+            }
         }
     },
 };
