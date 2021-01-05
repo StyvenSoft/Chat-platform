f8f6875224e02b74990a8632253d356e43f6ce87
Steveen <steveen.echeverri@gmail.com>
2021-01-05 17:29:32 -0500

    Created validations in register users resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,6 @@
 const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+const { UserInputError } = require('apollo-server');
 
 module.exports = {
     Query: {
@@ -14,8 +16,25 @@ module.exports = {
     },
     Mutation: {
         register: async (_, args) => {
-            const { username, email, password, confirmPassword } = args
+            let { username, email, password, confirmPassword } = args
+            let errors = {};
             try {
+                if(email.trim() === '') errors.email = 'Email must not be empty'
+                if(username.trim() === '') errors.username = 'Username must not be empty'
+                if(password.trim() === '') errors.password = 'Password must not be empty'
+                if(confirmPassword.trim() === '') errors.confirmPassword = 'Repeat password must not be empty'
+
+                if(password !== confirmPassword) errors.confirmPassword = 'Password must match'
+
+                const userByUsername = await User.findOne({ where: { username } });
+                const userByEmail = await User.findOne({ where: { email } });
+
+                if(userByUsername) errors.username = 'Username is taken'
+                if(userByEmail) errors.email = 'Email is taken'
+
+                if(Object.keys(errors).length > 0){ throw errors }
+
+                password = await bcrypt.hash(password, 6); 
                 // Create user
                 const user = await User.create({
                     username, 
@@ -26,7 +45,7 @@ module.exports = {
                 return user;
             } catch (error) {
                 console.log(error);
-                throw error;
+                throw new UserInputError('Bad input', { errors: error });
             }
         }
     }
