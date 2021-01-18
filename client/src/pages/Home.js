f70783c61a1e1df1cf4d89ea006f4acec7228388
Steveen <steveen.echeverri@gmail.com>
2021-01-18 18:33:14 -0500

    Created query getUsers in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,70 @@
-import React from 'react'
-import { Row, Button } from 'react-bootstrap';
+import { gql, useQuery } from '@apollo/client';
+import React, { Fragment } from 'react';
+import { Row, Button, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useAuthDispatch } from '../context/auth';
+
+const GET_USERS = gql`
+    query getUsers{
+        getUsers{
+            username
+            email
+            createdAt
+        }
+    }
+`
 
 export default function Home(props) {
     const dispatch = useAuthDispatch();
 
     const logout = () => {
-        dispatch({ type: 'LOGOUT'})
+        dispatch({ type: 'LOGOUT' })
         props.history.push('/login')
     }
 
+    const { loading, data, error } = useQuery(GET_USERS);
+
+    if (error) {
+        console.log(error)
+    }
+    if (data) {
+        console.log(data);
+    }
+
+    let usersMarkup;
+    if (!data || loading) {
+        usersMarkup = <p>Loading...</p>
+    } else if (data.getUsers.length === 0) {
+        usersMarkup = <p>Not users have joined yet</p>
+    } else if (data.getUsers.length > 0) {
+        usersMarkup = data.getUsers.map((user) => (
+            <div key={user.username} >
+                <p>{user.username}</p>
+            </div>
+        ))
+    }
+
     return (
-        <Row className="bg-white justify-content-around">
-            <Link to="/login">
-                <Button variant="link">Login</Button>
-            </Link>
-            <Link to="/register">
-                <Button variant="link">Register</Button>
-            </Link>
-            <Link to="/login">
-                <Button variant="link" onClick={logout}>Logout</Button>
-            </Link>
-        </Row>
+        <Fragment>
+            <Row className="bg-white justify-content-around mb-1">
+                <Link to="/login">
+                    <Button variant="link">Login</Button>
+                </Link>
+                <Link to="/register">
+                    <Button variant="link">Register</Button>
+                </Link>
+                <Link to="/login">
+                    <Button variant="link" onClick={logout}>Logout</Button>
+                </Link>
+            </Row>
+            <Row className="bg-white">
+                <Col xs={4}>
+                    {usersMarkup}
+                </Col>
+                <Col xs={8}>
+                    Messages
+                </Col>
+            </Row>
+        </Fragment>
     )
 }
