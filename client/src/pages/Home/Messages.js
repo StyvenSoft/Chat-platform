bad0a3581c5790552b31e5236b5832ccf9dbd077
Steveen <steveen.echeverri@gmail.com>
2021-01-28 23:43:26 -0500

    First part created Form text messages

--- client/src/pages/Home/Messages.js
+++ client/src/pages/Home/Messages.js
@@ -1,8 +1,20 @@
-import { gql, useLazyQuery } from '@apollo/client';
-import React, { Fragment, useEffect } from 'react';
-import { Col } from 'react-bootstrap';
+import { gql, useLazyQuery, useMutation } from '@apollo/client';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Col, Form } from 'react-bootstrap';
 import { useMessageDispatch, useMessageState } from '../../context/message';
 import Message from './Message';
+
+const SEND_MESSAGE = gql`
+    mutation sendMessage($to: String!, $content: String!) {
+        sendMessage(to: $to, content: $content) {
+            uuid
+            from 
+            to
+            content
+            createdAt
+        }
+    }
+`
 
 const GET_MESSAGES = gql`
     query getMessages($from: String!) {
@@ -19,10 +31,18 @@ const GET_MESSAGES = gql`
 export default function Messages() {
     const { users } = useMessageState()
     const dispatch = useMessageDispatch()
+    const [content, setContent] = useState('')
     const selectedUser = users?.find((u) => u.selected === true)
     const messages = selectedUser?.messages
 
-    const [getMessages, { loading: messagesLoading, data: messagesData }] = useLazyQuery(GET_MESSAGES);
+    const [getMessages, { 
+        loading: messagesLoading, data: messagesData 
+    }] = useLazyQuery(GET_MESSAGES);
+    
+    const [sendMessage] = useMutation(SEND_MESSAGE, {
+        onError: err => console.log(err)
+    })
+
     useEffect(() => {
         if (selectedUser && !selectedUser.messages) {
             getMessages({ variables: { from: selectedUser.username } })
@@ -31,12 +51,21 @@ export default function Messages() {
 
     useEffect(() => {
         if (messagesData) {
-            dispatch({ type: 'SET_USER_MESSAGES', payload: {
-                username: selectedUser.username,
-                messages: messagesData.getMessages
-            }})
+            dispatch({
+                type: 'SET_USER_MESSAGES', payload: {
+                    username: selectedUser.username,
+                    messages: messagesData.getMessages
+                }
+            })
         }
     }, [messagesData])
+
+    const submitMessage = e => {
+        e.preventDefault()
+        if (content === '') return
+
+        // Mutation the sending the message
+    }
 
     let selectedChatMarkup;
 
@@ -48,11 +77,11 @@ export default function Messages() {
         selectedChatMarkup = messages.map((message, index) => (
             <Fragment key={message.uuid}>
                 <Message message={message} />
-                {index === message.length -1 && (
+                {index === message.length - 1 && (
                     <div className="invisible">
-                        <hr className="m-0"/>
+                        <hr className="m-0" />
                     </div>
-                )}      
+                )}
             </Fragment>
         ))
     } else if (messages.length === 0) {
@@ -62,6 +91,19 @@ export default function Messages() {
     return (
         <Col xs={10} md={8} className="message-box d-flex flex-column-reverse">
             {selectedChatMarkup}
+            <Form onSubmit={submitMessage}>
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        className="rounded-pill bg-secondary"
+                        placeholder="Text a message..."
+                        value={content}
+                        OnChange={e => setContent(e.target.value)}
+                    >
+
+                    </Form.Control>
+                </Form.Group>
+            </Form>
         </Col>
     )
 }
