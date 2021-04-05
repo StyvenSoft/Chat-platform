8e0edb0d1408873cf49a64633c0fd811aeb88121
Steveen <steveen.echeverri@gmail.com>
2021-04-04 21:54:09 -0500

    Change relation foreignKey message and Users

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -20,8 +20,8 @@ module.exports = gql`
         uuid: String!
         content: String!
         createdAt: String!
-        Message: Message!
-        User: User!
+        message: Message!
+        user: User!
     }
     type Query {
         getUsers: [User]! 
